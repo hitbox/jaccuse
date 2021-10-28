8a9fc38b0b9c4b46077e1122ff1f7d2653a6530a
Carl Harris <elgoogemail2007@gmail.com>
2021-10-28 04:28:54 -0400

    init

---
+++ jaccuse.py
@@ -0,0 +1,35 @@
+"""
+My take on Sweigart's J'Accuse!
+"""
+import argparse
+import random
+import time
+
+class JAccuse:
+
+    def __init__(self, seconds_to_solve):
+        self.seconds_to_solve = seconds_to_solve
+
+    def reset(self):
+        self.start_time = None
+        self.game_over_time = None
+        # XXX
+        # 2021-10-15: left off here, not feeling this but wanting to come back.
+        # nvim session in `.session`.
+
+    def run(self):
+        self.start_time = time.time()
+        game
+
+
+def main(argv=None):
+    """
+    """
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args(argv)
+
+    jaccuse = JAccuse()
+    jaccuse.reset()
+
+if __name__ == '__main__':
+    main()
